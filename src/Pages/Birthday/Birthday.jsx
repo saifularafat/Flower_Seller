751c5f7e37db1e365bf4d1c42f6bdfe675ae069c
saifularafat <saifularafat.info@gmail.com>
2023-11-26 23:14:44 +0600

    birthday page top title and description is completet

--- src/Pages/Birthday/Birthday.jsx
+++ src/Pages/Birthday/Birthday.jsx
@@ -3,9 +3,22 @@ import PageTitleAndDescription from "../../components/PageTitleAndDescription";
 
 const Birthday = () => {
     return (
-        <div>
+        <div className="">
             <OfferTitle />
-            <PageTitleAndDescription />
+            <PageTitleAndDescription
+            path="/birthday-flower"
+            name="Birthday"
+            pageTitle="Birthday Flower"
+            title="Happy Birthday Flowers"
+            borderBG="bg-slate-400"
+            des1="Our birthday flowers include fresh roses, daisies, and more! Whether your happy birthday flower delivery is sent to home or office, Need your birthday flowers today? "
+            textLink1="birthday-flower"
+            linkName1="same-day-birthday-delivery"
+            des2="is available! With"
+            textLink2="birthday-flower"
+            linkName2="same-day-birthday-delivery"
+            des3=" on orders placed by 2PM, no one has to know whether you ordered weeks ago or this morning. "
+            />
         </div>
     );
 };
--- src/components/PageTitleAndDescription.jsx
+++ src/components/PageTitleAndDescription.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import PagesPath from "./PagesPath";
 
-const PageTitleAndDescription = ({ path, name, pageTitle, title, borderBG, des1, textLink1, linkName1,des2, textLink2, linkName2, des3 }) => {
+const PageTitleAndDescription = ({ path, name, pageTitle, title, bgBorder, des1, textLink1, linkName1,des2, textLink2, linkName2, des3 }) => {
     return (
         <>
             <PagesPath path={path} name={name} title={pageTitle} />
@@ -10,12 +10,12 @@ const PageTitleAndDescription = ({ path, name, pageTitle, title, borderBG, des1,
                     <div className="absolute translate-y-1/2 pt-1">
                         <h3 className="text-2xl font-semibold text-blue-950">{title}</h3>
                     </div>
-                    <div className={`absolute h-full w-[2px] ${borderBG} right-6 top-0`}>
+                    <div className={`absolute h-full w-[2px] bg-slate-300 ${bgBorder} right-6 top-0`}>
                     </div>
                 </div>
                 <div className="col-span-2 md:col-span-5 ">
                     <p className="">
-                        {des1}<Link to={textLink1}>{linkName1}</Link>{des2}<Link to={textLink2}>{linkName2}</Link>{des3}
+                        {des1} <Link to={textLink1} className="text-blue-500 underline">{linkName1}</Link> {des2} <Link to={textLink2} className="text-blue-500 underline"> {linkName2}</Link>{des3}
                     </p>
                 </div>
             </div>

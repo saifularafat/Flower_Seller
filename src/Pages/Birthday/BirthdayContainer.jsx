5da8f4ab9580ba41dc712fb9232a9471f1211ce7
saifularafat <saifularafat.info@gmail.com>
2023-11-27 23:23:23 +0600

    card is complete design

--- src/Pages/Birthday/BirthdayContainer.jsx
+++ src/Pages/Birthday/BirthdayContainer.jsx
@@ -3,6 +3,17 @@ import img1 from "../../assets/birthdayImages/Birthday_Wishes_Flower_Cake_Vibran
 import img2 from "../../assets/birthdayImages/Cart_Gift.webp"
 import img3 from "../../assets/birthdayImages/Denarii_Lavender_SpaBasket.webp"
 import img4 from "../../assets/birthdayImages/Happy_Birthday_Kid.webp"
+import img5 from "../../assets/birthdayImages/Animated_Happy_Birthday_Gift_BundlePassport_Eligible_Animated_Happy_Birthday_Gift_Bundle.webp"
+import img6 from "../../assets/birthdayImages/Conversation_Roses_Happy_BirthdayPassport_Eligible_Conversation_Roses_Happy_Birthday.webp"
+import img7 from "../../assets/birthdayImages/B-day_gift.webp"
+import img8 from "../../assets/birthdayImages/Animated_Birthday_Dog_Gift.webp"
+import img9 from "../../assets/birthdayImages/Happy_Birthday_Sweets_Gift_Basket.webp"
+import img10 from "../../assets/birthdayImages/Holiday_Lily_Bouquet.webp"
+import img11 from "../../assets/birthdayImages/Jumbo_Birthday.webp"
+import img12 from "../../assets/birthdayImages/mk033136x.webp"
+import img13 from "../../assets/birthdayImages/Shore_Passport-Eligible_Mickey_&Pluto_Birthday_by_Jim_Shore.webp"
+import img14 from "../../assets/birthdayImages/Warm_Sunset_BouquetPassport_Eligible_Warm_Sunset_Bouquet.webp"
+import img15 from "../../assets/birthdayImages/HappyBirthdayDishGarden.webp"
 const BirthdayContainer = () => {
     return (
         <div className="grid grid-cols-3 gap-5 ">
@@ -32,6 +43,118 @@ const BirthdayContainer = () => {
                 <div className=" px-1">
                     <h4 className="text-lg font-semibold leading-tight">Happy Birthday Kid</h4>
                     <p className="text-lg font-bold">$55.40 - $63.60</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img5} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Animated Happy Birthday Gift</h4>
+                    <p className="text-lg font-bold">$60.00 - $68.90</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img6} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Conversation Roses Happy Birthday</h4>
+                    <p className="text-lg font-bold">$63.70 - $71.10</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img7} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">B-Day Gift</h4>
+                    <p className="text-lg font-bold">$49.99 - $56.70</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img2} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Party Pooch™</h4>
+                    <p className="text-lg font-bold">$58.99 - $67.60</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img3} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Denari Lavender SpaBasket</h4>
+                    <p className="text-lg font-bold">$48.99 - $56.00</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img8} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Animated Birthday Dog Gift</h4>
+                    <p className="text-lg font-bold">$65.10 - $73.60</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img9} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Happy Birthday Sweets Gift Basket</h4>
+                    <p className="text-lg font-bold">$57.80 - $62.20</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img10} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Holiday Lily Bouquet</h4>
+                    <p className="text-lg font-bold">$55.10 - $59.99</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img11} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Jumbo Birthday</h4>
+                    <p className="text-lg font-bold">$53.99 - $59.77</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img12} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Birthday Cake Explosion Box </h4>
+                    <p className="text-lg font-bold">$69.69 - $77.77</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img13} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight"> Mickey & Pluto Birthday</h4>
+                    <p className="text-lg font-bold">$58.29 - $61.50</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img14} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Warm Sunset Bouquet</h4>
+                    <p className="text-lg font-bold">$65.99 - $ 75.69</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img15} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Happy Birthday Dish Garden </h4>
+                    <p className="text-lg font-bold">$54.80 - $60.20</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img8} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Animated Birthday Dog Gift</h4>
+                    <p className="text-lg font-bold">$65.10 - $73.60</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img9} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Happy Birthday Sweets Gift Basket</h4>
+                    <p className="text-lg font-bold">$57.80 - $62.20</p>
+                </div>
+            </Link>
+            <Link to="" className="w-full h-[400px] space-y-3 hover:shadow-md transition-all duration-200 rounded overflow-hidden">
+                <img src={img10} alt="" className="w-full h-80 object-cover" />
+                <div className=" px-1">
+                    <h4 className="text-lg font-semibold leading-tight">Holiday Lily Bouquet</h4>
+                    <p className="text-lg font-bold">$55.10 - $59.99</p>
                 </div>
             </Link>
 

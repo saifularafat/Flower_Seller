6e0193101be374763df1405dbe0b3f48cfe1b850
saifularafat <saifularafat.info@gmail.com>
2024-02-27 00:01:00 +0600

    Communiry second section

--- src/Pages/Community/Community.jsx
+++ src/Pages/Community/Community.jsx
@@ -1,5 +1,9 @@
 import PagesPath from "../../components/PagesPath";
-import banner from "../../assets/banner/communities-hero.webp"
+import { MdSlowMotionVideo } from "react-icons/md";
+import banner from "../../assets/community/communities-hero.webp"
+import Alice from "../../assets/community/alices-table.webp"
+import floral from "../../assets/community/floral-heart-project.webp"
+import ceo from "../../assets/community/communities-hero.webp"
 
 const Community = () => {
     return (
@@ -19,26 +23,42 @@ const Community = () => {
                 </p>
             </div>
             <div className="py-5 relative">
-                <img src={banner} alt="" />
+                <img src={banner} alt="" className="md:h-[420px] w-full object-cover" />
                 <div className="absolute top-[30%] left-[20%] ">
                     <div>
-                        <span className="text-sm md:text-3xl font-serif leading-4 font-semibold text-center text-white">Make Every Birthday</span>
+                        <span className="text-base md:text-3xl font-serif leading-4 font-semibold text-center text-white">Make Every Birthday</span>
                     </div>
                     <div>
-                        <span className="text-sm md:text-3xl font-serif leading-4 font-semibold text-center text-white">Special With a Free</span>
+                        <span className="text-base md:text-3xl font-serif leading-4 font-semibold text-center text-white">Special With a Free</span>
                     </div>
                     <div>
-                        <span className="text-sm md:text-3xl font-serif leading-4 font-semibold text-center text-white">Group Video Montage!</span>
+                        <span className="text-base md:text-3xl font-serif leading-4 font-semibold text-center text-white">Group Video Montage!</span>
                     </div>
-                    <div className="text-sm font-normal text-white pt-3">
+                    <div className="text-sm font-normal text-white md:pt-3 pt-1">
                         <p>We've teamed up with Tribute.co to extend this free offer</p>
                         <p>exclusively for 1-800-Flowers & Family of Brands Customers!</p>
                     </div>
-                    <div className="pt-3">
+                    <div className="md:pt-5 pt-2">
                         <button className="text-slate-800 bg-slate-50 rounded-lg md:py-2 py-1 px-3 md:px-6 md:text-base text-sm md:font-bold font-medium font-sans-">
                             START A TRIBUTE
                         </button>
                     </div>
+                    <div className="md:pt-9 pt-4 flex items-center gap-2 text-white cursor-pointer">
+                        <MdSlowMotionVideo />
+                        <span>Watch Sample Video</span>
+                    </div>
+                </div>
+            </div>
+            {/* second section */}
+            <div className="grid md:grid-cols-3 col-span-1 gap-5">
+                <div className="col-span-1">
+                    <img src={Alice} alt="" />
+                </div>
+                <div className="col-span-1">
+
+                </div>
+                <div className="col-span-1">
+                    <img src={floral} alt="" />
                 </div>
             </div>
         </div>

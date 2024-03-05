d7d213feb9bb68e9207f7cdb39e29894a2417c4d
saifularafat <saifularafat.info@gmail.com>
2024-03-06 00:01:27 +0600

    user dashboard order and payment page design create and design done

--- src/Pages/Dashboard/Users/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/Dashboard/Users/PaymentHistory/PaymentHistory.jsx
@@ -4,9 +4,32 @@ const PaymentHistory = () => {
     return (
         <div>
             <Helmet>
-                <title>Flower Shop || Payment History </title>
+                <title>Payment History || Flower Shop </title>
             </Helmet>
-            <h3>Payment History</h3>
+            <div className="overflow-x-auto">
+                <table className="min-w-[100%] shadow-md border mx-auto border-gray-100 my-2">
+                    <thead>
+                        <tr className="bg-[#5d90b4] text-white">
+                            <th className="py-3 px-4 text-base text-left border-b">Product Image</th>
+                            <th className="py-3 px-4 text-base text-left border-b">Product Name</th>
+                            <th className="py-3 px-4 text-base text-left border-b">Price</th>
+                            <th className="py-3 px-4 text-base border-b text-end">Transition Id</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="hover:bg-gray-50 border-b transition duration-300">
+                            <td className="py-4 px-4 flex justify-start">
+                                <img src="https://source.unsplash.com/64x64/?phone" alt="table navigate ui" className="h-14 w-14 object-cover bg-gray-300" />
+                            </td>
+                            <td className="py-4 px-6 border-b text-base font-semibold">Samsung s22</td>
+                            <td className="py-4 px-6 border-b text-base font-semibold">$599.99</td>
+                            <td className="py-4 px-6 border-b text-end text-base">
+                               <h2>sdfdkfngvdjkb</h2>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };

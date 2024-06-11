78872fb395db5761729db9e28bfe022f66143ffd
saifularafat <saifularafat.info@gmail.com>
2024-06-11 23:28:12 +0600

    spam email simple desgin done

--- src/Pages/Dashboard/Admin/EmailAdmin/EmailPage/SpamEmail/SpamEmail.jsx
+++ src/Pages/Dashboard/Admin/EmailAdmin/EmailPage/SpamEmail/SpamEmail.jsx
@@ -1,8 +1,89 @@
 
+import { GrCheckbox } from "react-icons/gr";
+import { ImCheckboxChecked } from "react-icons/im";
+import { useState } from "react";
+import { BiErrorAlt, BiTrash } from "react-icons/bi";
+import { HiOutlineMail } from "react-icons/hi";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { Tooltip } from "react-tooltip";
+import EmailTopTableIcon from "../../EmailRightSite/EmailTopTableIcon";
+import EmailUserInfo from "../../EmailRightSite/EmailUserInfo";
+import ModalFirstPart from "../../EmailRightSite/ModalFirstPart";
+import ModalSecondPart from "../../EmailRightSite/ModalSecondPart";
+import ModalMainEmailShow from "../../EmailRightSite/ModalMainEmailShow";
+import { useEmailAll } from "../../../../../../api/useEmailAll";
+import useAuth from "../../../../../../api/useAuth";
+
 const SpamEmail = () => {
+    const [select, setSelect] = useState(false);
+    const [spin, setSpin] = useState(false);
+    const [dotClick, setDotClick] = useState(false);
+    const [startClick, setStartClick] = useState(false);
+    const [emailClick, setEmailClick] = useState(false);
+
+    const [emails, refetch, isLoading] = useEmailAll();
+    const { user } = useAuth();
+    const toEmailFilter = emails.filter(email => email?.toEmail === user?.email)
+
+    // const indexEmailFilter = emails.filter(email => email?.toEmail === user?.email)
+    console.log("index", toEmailFilter);
+    // const emailFilter = users.filter(emailOne => emailOne?.email === findEmail?.sendEmail )
+
     return (
-        <div>
-            
+        <div className={`h-[580px] text-accent rounded-r-xl shadow-2xl relative`}>
+            {/* table icon */}
+            <EmailTopTableIcon
+                select={select}
+                setSelect={setSelect}
+                spin={spin}
+                setSpin={setSpin}
+                dotClick={dotClick}
+            />
+            {/* Main Content table */}
+            <div className="overflow-y-scroll h-[380px] myEmailScroll">
+                {/* first */}
+                <div>
+                    {/* email table */}
+                    {
+                        toEmailFilter.map(email =>
+                            <button key={email?._id}
+                                onClick={() => setSelect(!select)}
+                                className={`flex items-center justify-between gap-6 w-full py-3 border-0 border-b border-solid border-slate-400 hover:scale-y-100 transition-all duration-200`}>
+                                <div className="flex items-center gap-2 px-2">
+                                    <button className="" >
+                                        {
+                                            select ?
+                                                <ImCheckboxChecked />
+                                                :
+                                                <GrCheckbox />
+                                        }
+
+                                    </button>
+
+
+                                    <button className="text-slate-800 border-none" onClick={() => setStartClick(!startClick)}>
+
+                                        {email?.starred === "start" ?
+                                            <AiFillStar className="text-xl text-yellow-500" />
+                                            :
+                                            <AiOutlineStar className="text-xl " />
+                                        }
+                                    </button>
+                                    {/* Show Email table */}
+                                    <EmailUserInfo email={email} />
+                                </div>
+                                <div className="flex items-center gap-4 mx-3">
+                                    <Tooltip id="my-tooltip" place="top" />
+                                    <BiTrash className="text-xl text-red-700 border-none" data-tooltip-id="my-tooltip" data-tooltip-content="Delete Mail" />
+                                    <HiOutlineMail className="text-xl text-blue-500" data-tooltip-id="my-tooltip" data-tooltip-content="Mark as Unread" />
+                                    <BiErrorAlt className="text-xl text-slate-700" data-tooltip-id="my-tooltip" data-tooltip-content="Mark to spam" />
+                                </div>
+                            </button>
+                        )
+                    }
+                   
+                </div>
+            </div>
         </div>
     );
 };
